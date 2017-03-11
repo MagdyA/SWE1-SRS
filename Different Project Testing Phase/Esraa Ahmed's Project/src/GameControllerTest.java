b4f8abddf05eef3a9d3dd32bf18c26abc667c884
amrshosny <amrshosny@gmail.com>
2017-03-11 20:36:35 +0200

    Add files via upload

--- Different Project Testing Phase/Esraa Ahmed's Project/src/GameControllerTest.java
+++ Different Project Testing Phase/Esraa Ahmed's Project/src/GameControllerTest.java
@@ -9,15 +9,28 @@ import org.testng.annotations.Test;
 public class GameControllerTest {
 
 	GameController gameController = new GameController();
+	
 
 	@BeforeTest
-	public void prepareAddGameInputSouce() throws FileNotFoundException {
+	public void prepareAddGameRequiredData() throws FileNotFoundException {
 		gameController.in = new Scanner(new File("AddGame Input.txt"));
+		Category math = new Category(); 
+		math.CatName = "Math";
+		Category science = new Category();
+		science.CatName = "Science";
+		gameController.Categories.add(math);
+		gameController.Categories.add(science);		
 	}
 
 	@Test(invocationCount = 4)
 	public void AddGame() {
 		gameController.AddGame();
+		for ( int i = 0; i < gameController.Categories.size(); i++ ) {
+			System.out.println("Category: " + gameController.Categories.get(i).CatName);
+			for ( int j = 0 ; j < gameController.Categories.get(i).ListOfGames.size() ; j++) {
+				System.out.println("Game :" + gameController.Categories.get(i).ListOfGames.get(j).Name);
+			}
+		}
 	}
 
 	@Test
