46c6e846d62a016b38de9e317ea15dc3170c46bf
Ashrakat <ashrakat.mokhtar@yahoo.com>
2017-03-11 10:17:52 +0200

    Update AccountControllerTest.java

--- Different Project Testing Phase/Esraa Ahmed's Project/src/AccountControllerTest.java
+++ Different Project Testing Phase/Esraa Ahmed's Project/src/AccountControllerTest.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.testng.Assert;
@@ -9,59 +8,35 @@ import org.testng.annotations.Test;
 
 public class AccountControllerTest {
 	AccountController accountController = new AccountController();
-	
-//	@BeforeTest
-//	public void prepareSystemAccounts () throws FileNotFoundException {
-//		
-//		// Inserting Accounts Information from Users.txt into accountController's ArrayList of Accounts
-//		
-//		Scanner in = new Scanner(new File("Accounts.txt"));
-//		while(in.hasNext()) {
-//			Account account = new Account();
-//			account.Name = in.next();
-//			account.Age = in.nextInt();
-//			account.Email = in.next();
-//			account.Password = in.next();
-//			account.Gender = in.next();
-//			account.Type = in.next();
-//			accountController.Acc.add(account);
-//		}
-//		in.close();
-//		
-//	}
+	AccountController accountController1 = new AccountController();
 	
 	@BeforeTest
 	public void prepareSignUpInputSource () throws FileNotFoundException {
 		
-		// Reinitialize accountController's Scanner to Accept Input from Login Input.txt
-		accountController.in = new Scanner(new File("Accounts.txt"));		
-				
+		// Reinitialize accountController's Scanner to Accept Input from Accounts.txt
+		accountController1.in = new Scanner(new File("Accounts.txt"));
 	}
 
-//	@BeforeTest
-//	public void prepareLoginInputSource () throws FileNotFoundException {
-//		
-//		// Reinitialize accountController's Scanner to Accept Input from Login Input.txt
-//		accountController.in = new Scanner(new File("Login Input.txt"));
-//	}
-//	
-	
-	// InvocationCount Equals to the Number of Test Cases in Login Input.txt File
-	
-	ArrayList<Account> accounts = new ArrayList<Account>();
-	int i=0;
-	
-	@Test(invocationCount = 11 , priority = 0)
-	public void Register() {
-		Account NewMember = accountController.Registration();
-        accounts.add(NewMember);
+	@BeforeTest
+	public void prepareLoginInputSource () throws FileNotFoundException {
+		
+		// Reinitialize accountController's Scanner to Accept Input from Login Input.txt
+		accountController.in = new Scanner(new File("Login Input.txt"));
 	}
 	
+	// InvocationCount Equals to the Number of Test Cases in Login Input.txt File
+	@Test(invocationCount = 11)
+	public void Signup() {
+		Account NewMember = accountController1.Registration();
+        String type = accountController.Login(NewMember);
+		Assert.assertEquals(NewMember.Type, type);
+	}
 	
-	@Test(invocationCount = 36 , priority = 1)
+	@Test(invocationCount = 36)
 	public void Login() throws FileNotFoundException {
+		Account user = new Account();
+		accountController = new AccountController();
 		accountController.in = new Scanner(new File("Login Input.txt"));
-		Account user = accounts.get(i);
 		Assert.assertEquals(user.Type, accountController.Login(user));
 	}
 }
